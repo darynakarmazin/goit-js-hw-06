5476ef62c058c9bbe17ba4ce97ec3ff42292c4dd
Daryna Karmazin <daryna.karmazin@gmail.com>
2023-03-07 20:35:20 +0200

    Completed task 5-6

---
+++ js/task-06.js
@@ -0,0 +1,25 @@
+// Завдання 6
+// Напиши скрипт, який під час втрати фокусу на інпуті (подія blur), перевіряє його вміст щодо правильної кількості введених символів.
+
+// <input
+//   type="text"
+//   id="validation-input"
+//   data-length="6"
+//   placeholder="Please enter 6 symbols"
+// />
+
+// Яка кількість символів повинна бути в інпуті, зазначається в його атрибуті data-length.
+// Якщо введена правильна кількість символів, то border інпуту стає зеленим, якщо неправильна кількість - червоним.
+// Для додавання стилів використовуй CSS-класи valid і invalid, які ми вже додали у вихідні файли завдання.
+
+const inputEl = document.querySelector("#validation-input");
+
+inputEl.addEventListener("blur", checkAmountOfsymbols);
+
+function checkAmountOfsymbols(event) {
+  if (event.currentTarget.value.length == inputEl.dataset.length) {
+    inputEl.classList.add("valid");
+  } else {
+    inputEl.classList.add("invalid");
+  }
+}
