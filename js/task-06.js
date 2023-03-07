9eee1e4b4aac2f85441dba73fa30a430722f6bff
Daryna Karmazin <daryna.karmazin@gmail.com>
2023-03-07 20:39:10 +0200

    Completed task 5-6

--- js/task-06.js
+++ js/task-06.js
@@ -19,7 +19,9 @@ inputEl.addEventListener("blur", checkAmountOfsymbols);
 function checkAmountOfsymbols(event) {
   if (event.currentTarget.value.length == inputEl.dataset.length) {
     inputEl.classList.add("valid");
+    inputEl.classList.remove("invalid");
   } else {
     inputEl.classList.add("invalid");
+    inputEl.classList.remove("valid");
   }
 }
