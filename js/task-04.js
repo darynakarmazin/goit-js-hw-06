1524eb70efe6e20b3cfbcc0510e8321f0339c14e
Daryna Karmazin <daryna.karmazin@gmail.com>
2023-03-07 19:23:51 +0200

    Completed task 4

--- js/task-04.js
+++ js/task-04.js
@@ -9,3 +9,24 @@
 // Створи змінну counterValue, в якій буде зберігатися поточне значення лічильника та ініціалізуй її значенням 0.
 // Додай слухачів кліків до кнопок, всередині яких збільшуй або зменшуй значення лічильника.
 // Оновлюй інтерфейс новим значенням змінної counterValue.
+
+const decrementBtnEl = document.querySelector('button[data-action="decrement"');
+// console.log(decrementBtnEl);
+const incrementBtnEl = document.querySelector('button[data-action="increment"');
+// console.log(incrementBtnEl);
+const counterValueEl = document.querySelector("#value");
+// console.log(counterValueEl);
+
+decrementBtnEl.addEventListener("click", onValueDecrement);
+incrementBtnEl.addEventListener("click", onValueIncrement);
+
+let counterValue = 0;
+
+function onValueDecrement() {
+  counterValue -= 1;
+  counterValueEl.textContent = counterValue;
+}
+function onValueIncrement() {
+  counterValue += 1;
+  counterValueEl.textContent = counterValue;
+}
